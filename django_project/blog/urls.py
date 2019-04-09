93fcd00cd46dd0e6757f389b2fed796e4f1e6ae0
Anadi Sharma <anadisharma11041997@gmail.com>
2019-04-09 18:50:49 +0530

    changes

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView,UserPostListView
-from .views import home
+from .views import home,calc
 urlpatterns = [
     # path('',PostListView.as_view(),name='blog-home'),
     path('',home,name='blog-home'), # this gives best performance. displays all posts at once. Performed amazing in loadtest
@@ -10,6 +10,6 @@ urlpatterns = [
     path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
     path('post/new/',PostCreateView.as_view(),name='post-create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
-    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete')
     
 ]
